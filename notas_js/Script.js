c5f9250781acd2d2f4344b55a24c518e1dbeb6ab
Eddlash <edsondearco0630@gmail.com>
2022-11-19 22:13:39 -0500

    format

---
+++ notas_js/Script.js
@@ -0,0 +1,103 @@
+//asi colocaremos comentarios en javascript
+/*este es un comentario de varías
+líneas*/
+
+//VARIABLES
+/* en javascript se trabaja con valores y
+esos valores tienen un tipo específico de 
+datos dependiendo de su propósito
+dentro del código*/
+
+//TIPOS DE DATOS
+/* undefined: cuando no existe un valor asignado.
+null: cuando un objeto es nulo.
+boolean: valores buleanos (verdadero y falso)
+String: son cadenas de carácteres.
+symbol:
+number: representa un numero en el código.
+ibject: representa un objeto (propiedades y funciones que representan un objeto)*/
+
+/*VARIABLE
+crea un espacio en el dispositivo para gusrdar un valor específico
+en ese lugar de memoria.*/
+//ejemplo:
+var miNombre = "Edson";
+let
+const
+
+//OPERADOR DE ASIGNACIÓN
+var a;
+var b = 2;
+
+console.log(a) //ERROR 
+console.log(b) // tiene un valor.
+
+a = 10;
+console.log(a) // ya tiene un valor 
+// = es el operador de asignación en javascript.
+
+//ASIGNAR EL VALOR DE UNA VARIABLE A OTRA VARIABLE
+var a = 5;
+var b = a; //asignamos el valor de (a) como valor de la variable (b)
+var a = 5;
+var b;
+b = a; //asigna la variable en una liena distinta.
+
+// INICIALIZAR VARIABLES
+var x = 9; // inicializamos la variable x con el valor 9
+var miIdiona = "español";
+
+//VARIABLES NO INICIALIZADAS
+var a; // no es inicializada porque no tiene un valor 
+a = 56; // ahora si la variable tiene un valor
+
+
+//OPERACIONES ARITMÉTICAS
+//SUMA
+var suma = 7 + 12;
+console.log(suma) //da el resuletado de la suma.
+//RESTA
+var resta = 15 - 5;
+console.log(resta) //da el resultado de la resta 
+var resta = 5 - 15;
+console.log(resta) //el resultado en negativo 
+//MULTIPLICACIÓN
+var producto = 5 * 3;
+console.log(producto) //da el resultado de la multiplicación
+producto = 9 * 0; //actualizamos el valor de la variable
+//DIVISIÓN
+var cociente = 20 / 2;
+console.log(cociente) // mostrará el resultado de la división
+cociente = 17 / 31; //actualizamos el valor de la variable 
+
+// NÚMEROS DECIMALES
+var miNumeroDecimal = 24.4; //el punto nos indica que se encuentra un numero decimal
+var peso = 78.34;
+//MULTIPLICAR NÚMEROS DECIMALES
+var producto = 3.4 * 10.4; //se pueden multiplicar numeros decimales y el resultado sera un número decimal
+producto = 2.4 * 4;
+producto = 6 * 8.9;
+producto = 3.6 * 0;
+producto = -5.7 * 3.4; // se puede multiplicar un numero decimal con un numero entero
+//DIVIDIR NUMEROS DECIMALES
+var cociente = 5.0 / 2.0;
+//RESTO DE UNA DIVISIÓN
+var resto 15 % 5; /*el símbolo de porcentaje es el operador que nos
+permite obtener el resto de la división*/
+
+//INCREMENTAR EL VALOR DE UNA VARIABLE
+var librosComprados = 105; //inicialmente 
+// Opción 1
+librosComprados = librosComprados + 1;
+// Opción 2
+librosComprados++; // solo incrementa el valor de uno wn uno 
+//REDUCIR EL VALOR DE UNA VARIABLE
+
+
+
+
+
+
+
+
+
